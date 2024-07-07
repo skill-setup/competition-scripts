89bb3a1a3ade8167136bebf402c18d75fae7859b
Franz Stimpfl <franz.stimpfl@agilecoderz.com>
2024-07-07 07:09:06 +0200

    some changes

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# docker-compose -f network.yml up
-docker-compose -f gitea.yaml down
-docker-compose -f gitea-runner.yaml down
+docker compose -f gitea-runner.yaml down
+docker compose -f gitea.yaml down
+docker compose -f traefik.yaml down
 
 rm -rf ./data
