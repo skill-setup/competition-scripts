3852ae262bd057b95372afeb2ef9e24272ef19e2
Franz Stimpfl <franz.stimpfl@agilecoderz.com>
2024-10-30 08:21:45 +0100

    added git

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl git
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
