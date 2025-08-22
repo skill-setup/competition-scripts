4bcf7513679ccedaa8ca1edcc4ecfe4b81b5457a
Zoltan Sisak <zoltan.sisak@gmail.com>
2025-08-22 14:03:55 +0200

    Feat/add verdaccio (#9)
    * feat: add Verdaccio npm registry for package caching
    
    - Add verdaccio.yaml Docker Compose configuration
    - Integrate Verdaccio with Traefik reverse proxy
    - Add Verdaccio startup to init.sh with proper permissions
    - Include Verdaccio in start.sh and stop.sh scripts
    - Configure persistent storage and configuration volumes
    
    * refactor: remove environment variable export from init.sh

--- init.sh
+++ init.sh
@@ -161,6 +161,12 @@ MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD docker compose -f mysql.yaml up -d
 # Start watchtower
 USERNAME=$USERNAME PASSWORD=$PASSWORD DOMAIN=$DOMAIN docker compose -f watchtower.yaml up -d
 
+# Start Verdaccio for package caching
+docker compose -f verdaccio.yaml up -d
+
+# Configure Verdaccio storage permissions to allow package uploads
+chmod 777 -R ./data/verdaccio
+
 # Start competitors work
 docker compose -f competitors.yaml up -d 
 
--- stop.sh
+++ stop.sh
@@ -4,4 +4,4 @@ docker compose -f gitea-runner.yaml stop
 GITEA_HOSTNAME=$DOMAIN docker compose -f gitea.yaml stop 
 docker compose -f traefik.yaml stop 
 docker compose -f watchtower.yaml stop
-
+docker compose -f verdaccio.yaml stop
